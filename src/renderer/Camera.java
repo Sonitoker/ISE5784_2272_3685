87e464eed494c1efd89f7590926264384abe493e
odeyalapian <odeyah1515@gmail.com>
2024-06-02 14:25:29 +0300

    check

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -1,5 +1,63 @@
 package renderer;
 
-public class Camera {
-    int a;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+public class Camera implements Cloneable {
+
+    private Point p0;
+    private Vector vTo, vUp, vRight;
+    private double width = 0d, height = 0d, distance = 0d;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    private Camera() {
+    }
+
+    static public Builder getBuilder(){
+        return new Builder();
+    }
+
+    public Ray constructRay(int nX, int nY, int j, int i){
+        return null;
+    }
+
+    public static class Builder{
+        private final Camera camera= new Camera();
+        public Builder setP0(Point p0) {
+            camera.p0 = p0;
+            return this;
+        }
+
+        public Builder setDirection(Vector vTo, Vector vUp){
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
