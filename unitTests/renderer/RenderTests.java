854174d33a8636691b75c66b58813b7b15042355
Sonitoker <sonitoker@gmail.com>
2024-07-08 16:46:12 +0300

    end 7.3

--- unitTests/renderer/RenderTests.java
+++ unitTests/renderer/RenderTests.java
@@ -90,8 +90,8 @@ public class RenderTests {
         final Camera.Builder camera1 = Camera.getBuilder()
                 .setRayTracer(new SimpleRayTracer(scene))
                 .setLocation(new Point(0,10,0))
-                .setDirection(new Vector(0, 0, -1), new Vector(0,1,0))
-                .setVpDistance(130)
+                .setDirection(new Vector(0,0,-1), new Vector(0,1,0))
+                .setVpDistance(120)
                 .setVpSize(280, 280);
         assertDoesNotThrow(() -> {
             Scene scene = JsonScene.importScene("JsonScenes/sceneTrial.json");
@@ -105,7 +105,26 @@ public class RenderTests {
     }
 
 
+    @Test
+    public void basicRenderJson2() {
+        assertDoesNotThrow(() -> {
+                    final Camera.Builder camera = Camera.getBuilder()
+                            .setRayTracer(new SimpleRayTracer(scene))
+                            .setDirection(new Vector(0, 1, -0.1).normalize(), new Vector(0, 1, 10).normalize())
+                            .setLocation(new Point(0, -350, 45))//Point(0, 130, 30)
+                            .setVpDistance(500)
+                            .setVpSize(150, 150);
+                    Scene scene = JsonScene.importScene("jsonScenes/diamondScene.json");
 
+                    camera
+                            .setRayTracer(new SimpleRayTracer(scene))
+                            .setImageWriter(new ImageWriter("MINIP test", 1000, 1000))
+                            .build()
+                            .renderImage()
+                            .writeToImage();
+                }, "Failed to render image"
+        );
+    }
 
     /**
      * the bonus for stage 7, Produces a spectacular image with many bodies.
@@ -199,72 +218,5 @@ public class RenderTests {
                 .renderImage()
                 .writeToImage();
 
-//        final Camera.Builder bonuscameraBuilder = Camera.getBuilder()
-//                .setDirection(new Vector(1, 0, 0), new Vector(0, 0, 1));
-//
-//        scene.setAmbientLight(new AmbientLight(new Color(255, 255, 255).reduce(6), new Double3(0.15)));
-//        scene.lights.add(new SpotLight(new Vector(1, 0, 0), new Color(RED), new Point(-300, 6, 10)));
-//
-//        double angle = 0;
-//        double height = 0;
-//
-//        scene.geometries.add(new Plane(new Point(-4, 4, 0), new Vector(0, 0, 1))
-//                .setMaterial(new Material().setKd(0.8).setKs(0.6).setShininess(100).setKt(0.7).setKr(0.5)));
-//
-//        java.awt.Color[] colors = {YELLOW, RED, ORANGE, BLUE};
-//
-//        for (int i = 25; i < 200; ++i) {
-//            int colorIndex = i % colors.length;
-//
-//            scene.geometries
-//                    .add(new Sphere(0.5, new Point(i / 25.0 * Math.cos(angle), i / 25.0 * Math.sin(angle), height))
-//                            .setEmission(new Color(colors[colorIndex]).reduce(2.2))
-//                            .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(80).setKt(0.3)));
-//
-//            angle += Math.PI / 15.0;
-//            height += 0.15;
-//        }
-//
-//        java.awt.Color[] colors2 = {BLUE, GREEN, PINK, BLACK, RED, GRAY};
-//
-//        height = 10;
-//        for (int i = 25; i < 100; ++i) {
-//            int colorIndex = i % colors2.length;
-//
-//            scene.geometries.add(new Sphere(0.3,new Point(i / 25.0 * Math.cos(angle), i * Math.sin(angle), height))
-//                    .setEmission(new Color(colors2[colorIndex]).reduce(2.2))
-//                    .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(80).setKt(0.3)));
-//
-//            angle += Math.PI / 30.0;
-//            height += 0.5;
-//        }
-//
-//        height = 10;
-//        for (int i = 25; i < 300; ++i) {
-//
-//            scene.geometries.add(new Sphere(0.05, new Point(i / 25.0 * Math.cos(angle), i * Math.sin(angle), height))
-//                    .setEmission(new Color(WHITE))
-//                    .setMaterial(new Material().setKd(1).setKs(1d).setShininess(100).setKt(1)));
-//
-//            angle += Math.PI / 60.0;
-//            height += 0.1 % 50;
-//        }
-//
-//        scene.lights.add(new SpotLight( new Vector(1, 0, 0),new Color(255, 255, 255).reduce(2), new Point(-150, 0, 5)));
-//        scene.lights.add(new SpotLight(new Vector(1, 0, 0),new Color(GREEN).reduce(2), new Point(50, 0, 5)));
-//
-//        scene.setBackground(new Color(BLUE).reduce(TRANSLUCENT));
-//
-//        ImageWriter imageWriter = new ImageWriter("myShapeBonus", 500, 500);
-//
-//        bonuscameraBuilder.setLocation(new Point(-330, 0, 5))
-//                .setVpDistance(1000)
-//                .setVpSize(200, 200)
-//                .setRayTracer(new SimpleRayTracer(scene))
-//                .setImageWriter(imageWriter)
-//                .build()
-//                .renderImage()
-//                .writeToImage();
-//    }
     }
 }
